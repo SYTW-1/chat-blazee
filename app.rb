b33f4b222ad981aacb4bcd2de96913fe94fa8373
alu0100611298 <alu0100611298@ull.edu.es>
2014-11-15 12:36:04 +0000

    vista chat

--- app.rb
+++ app.rb
@@ -17,6 +17,10 @@ get '/' do
   end
 end
 
+get '/chat' do
+  erb :chat
+end
+
 post '/' do
   if(user.include?(params[:username]))
     redirect '/'
@@ -63,5 +67,33 @@ get '/user' do
         <%= phrase %> <br />
       <% end %>
     </div>
+  HTML
+end
+
+get '/chat/update' do
+  return [404, {}, "Not an ajax request"] unless request.xhr?
+  @updates = chat[params['last'].to_i..-1] || []
+
+  @last = chat.size
+  erb <<-'HTML', :layout => false
+      <% @updates.each do |phrase| %>
+          <ul class="chat">
+            <li class="left clearfix"><span class="chat-img pull-left">
+                <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+            </span>
+                <div class="chat-body clearfix">
+                    <div class="header">
+                        <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                            <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                    </div>
+                    <p>
+                        <%= phrase %> <br />
+                    </p>
+                </div>
+            </li>
+        </ul>
+      <% end %>
+
+      <span id="last" data-last="<%= @last %>"></span>
   HTML
 end
---
+++ public/js/chat.js
@@ -0,0 +1,22 @@
+$('#text').keypress(
+  function(e){
+    if( e.keyCode==13 ){
+      $.get('/send',{text:$('#text').val()});
+      $('#text').val('');
+    } 
+  }
+);
+
+(function() {
+  var last = 0;
+  setInterval(
+    function(){
+      $.get('/chat/update',{last:last},
+        function(response){
+          last = $($.parseHTML(response)).filter("#last").data('last');
+          $('#chat').append(response);
+        }
+      );
+    },
+  1000);
+})();
